29223a610a0e58199ed35a5b19df2554594dc57c
milan <milanzivanov@users.noreply.github.com>
2025-06-13 23:45:54 +0200

    absolute URL

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -42,17 +42,20 @@ export async function createCheckoutSession(
       customerId = customer.data[0].id;
     }
 
+    // Use absolute URL in production, relative URL in development
     const baseUrl =
       process.env.NODE_ENV === "production"
-        ? `https://${process.env.VERCEL_URL}`
-        : process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+        ? "https://ecommerce-app-eosin-seven.vercel.app"
+        : "http://localhost:3000";
+
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+    console.log("Base URL:", baseUrl);
 
     const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
-
     const cancelUrl = `${baseUrl}/basket`;
 
-    // console.log("//////////// successUrl", successUrl);
-    // console.log("//////// ///// cancelUrl", cancelUrl);
+    console.log("Success URL:", successUrl);
+    console.log("Cancel URL:", cancelUrl);
 
     // session
     const session = await stripe.checkout.sessions.create({
